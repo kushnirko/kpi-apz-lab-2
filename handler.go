0d350278a650e2908d8cecc6474333b14dbbd6a9
Nazg0r <gorchinshiy83@gmail.com>
2024-03-20 20:17:23 +0200

    Implement ComputeHandler struct

--- handler.go
+++ handler.go
@@ -1,12 +1,41 @@
 package lab2
 
+import (
+	"bufio"
+	"io"
+)
+
 // ComputeHandler should be constructed with input io.Reader and output io.Writer.
 // Its Compute() method should read the expression from input and write the computed result to the output.
 type ComputeHandler struct {
-	// TODO: Add necessary fields.
+	input  io.Reader
+	output io.Writer
 }
 
 func (ch *ComputeHandler) Compute() error {
-	// TODO: Implement.
+	bufReader := bufio.NewReader(ch.input)
+	bufWriter := bufio.NewWriter(ch.output)
+
+	postfix, err := bufReader.ReadString('\n')
+	if err != nil {
+		return err
+	}
+
+	//TODO change function to desired
+	prefix, err := PrefixToPostfix(postfix)
+	if err != nil {
+		return err
+	}
+
+	_, err = bufWriter.WriteString(prefix)
+	if err != nil {
+		return err
+	}
+
+	err = bufWriter.Flush()
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
