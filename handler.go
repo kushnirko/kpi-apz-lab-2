25ccec03834ced6699a50b86c5ec73933ce63de5
Nazg0r <gorchinshiy83@gmail.com>
2024-03-20 22:32:25 +0200

    Fix ComputerHandler struct

--- handler.go
+++ handler.go
@@ -8,34 +8,24 @@ import (
 // ComputeHandler should be constructed with input io.Reader and output io.Writer.
 // Its Compute() method should read the expression from input and write the computed result to the output.
 type ComputeHandler struct {
-	input  io.Reader
-	output io.Writer
+	Input  io.Reader
+	Output io.Writer
 }
 
 func (ch *ComputeHandler) Compute() error {
-	bufReader := bufio.NewReader(ch.input)
-	bufWriter := bufio.NewWriter(ch.output)
+	bufReader := bufio.NewReader(ch.Input)
+	bufWriter := bufio.NewWriter(ch.Output)
 
 	postfix, err := bufReader.ReadString('\n')
-	if err != nil {
-		return err
-	}
 
 	//TODO change function to desired
-	prefix, err := PrefixToPostfix(postfix)
+	prefix, err := PostfixToPrefix(postfix)
 	if err != nil {
 		return err
 	}
 
-	_, err = bufWriter.WriteString(prefix)
-	if err != nil {
-		return err
-	}
-
-	err = bufWriter.Flush()
-	if err != nil {
-		return err
-	}
+	bufWriter.WriteString(prefix)
+	bufWriter.Flush()
 
 	return nil
 }
