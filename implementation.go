bf4ed2612cad10027769f5e27d8349f4daae9214
Maksym Yatsenko <saddas1rambler@gmail.com>
2024-03-21 14:57:20 +0200

    add error handling to PostfixToPrefix function

--- implementation.go
+++ implementation.go
@@ -2,10 +2,14 @@ package lab2
 
 import (
 	"fmt"
+	"strconv"
 	"strings"
 )
 
 func PostfixToPrefix(postfixExpression string) (string, error) {
+	if postfixExpression == "" {
+		return "", fmt.Errorf("expression not found")
+	}
 
 	operators := map[string]bool{
 		"+": true,
@@ -18,7 +22,16 @@ func PostfixToPrefix(postfixExpression string) (string, error) {
 
 	tokens := strings.Split(postfixExpression, " ")
 	for _, token := range tokens {
+		if token == "" {
+			return "", fmt.Errorf("postfix expression entered incorrectly")
+		}
+		if !operators[token] && !isNumeric(token) {
+			return "", fmt.Errorf("invalid character entered")
+		}
 		if operators[token] {
+			if len(stack) < 2 {
+				return "", fmt.Errorf("postfix expression entered incorrectly")
+			}
 			operand2 := stack[len(stack)-1]
 			stack = stack[:len(stack)-1]
 			operand1 := stack[len(stack)-1]
@@ -29,5 +42,14 @@ func PostfixToPrefix(postfixExpression string) (string, error) {
 		}
 	}
 
+	if len(stack) != 1 {
+		return "", fmt.Errorf("postfix expression entered incorrectly")
+	}
+
 	return stack[0], nil
+}
+
+func isNumeric(token string) bool {
+	_, err := strconv.ParseFloat(token, 64)
+	return err == nil
 }
