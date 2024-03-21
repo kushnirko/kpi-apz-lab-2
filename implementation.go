eca7b0dda03572bee076a0b63380eb33d82f7d15
Maksym Yatsenko <saddas1rambler@gmail.com>
2024-03-21 14:53:42 +0200

    add PostfixToPrefix function to the implemetation.go

--- implementation.go
+++ implementation.go
@@ -1,9 +1,33 @@
 package lab2
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
-// TODO: document this function.
-// PrefixToPostfix converts
-func PrefixToPostfix(input string) (string, error) {
-	return "TODO", fmt.Errorf("TODO")
+func PostfixToPrefix(postfixExpression string) (string, error) {
+
+	operators := map[string]bool{
+		"+": true,
+		"-": true,
+		"*": true,
+		"/": true,
+		"^": true,
+	}
+	var stack []string
+
+	tokens := strings.Split(postfixExpression, " ")
+	for _, token := range tokens {
+		if operators[token] {
+			operand2 := stack[len(stack)-1]
+			stack = stack[:len(stack)-1]
+			operand1 := stack[len(stack)-1]
+			stack = stack[:len(stack)-1]
+			stack = append(stack, fmt.Sprintf("%s %s %s", token, operand1, operand2))
+		} else {
+			stack = append(stack, token)
+		}
+	}
+
+	return stack[0], nil
 }
