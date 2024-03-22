cac3746083e7956298b7f8e46fbd4e7a3e051083
Mykola Kushnir <mykola.kushnir.825@gmail.com>
2024-03-22 17:29:59 +0200

    Refactor and unify tests

--- implementation_test.go
+++ implementation_test.go
@@ -1,72 +1,85 @@
 package lab2
 
 import (
+	"errors"
 	"fmt"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
 func TestPostfixToPrefix(t *testing.T) {
-	result, err := PostfixToPrefix("")
-	assert.Equal(t, "", result)
-	assert.EqualError(t, err, "expression not found")
-
-	result, err = PostfixToPrefix("4 2 7 8 ! ? @")
-	assert.Equal(t, "", result)
-	assert.EqualError(t, err, "invalid character entered")
-
-	result, err = PostfixToPrefix("8 5+")
-	assert.Equal(t, "", result)
-	assert.EqualError(t, err, "invalid character entered")
-
-	result, err = PostfixToPrefix("4 2 - + -")
-	assert.Equal(t, "", result)
-	assert.EqualError(t, err, "postfix expression entered incorrectly")
-
-	result, err = PostfixToPrefix("4 2 5")
-	assert.Equal(t, "", result)
-	assert.EqualError(t, err, "postfix expression entered incorrectly")
-
-	result, err = PostfixToPrefix("4.1 2.5 +")
-	assert.Equal(t, "+ 4.1 2.5", result)
-	assert.NoError(t, err)
-
-	result, err = PostfixToPrefix("6 2 * 3 -")
-	assert.Equal(t, "- * 6 2 3", result)
-	assert.NoError(t, err)
-
-	result, err = PostfixToPrefix("41 23 10 / +")
-	assert.Equal(t, "+ 41 / 23 10", result)
-	assert.NoError(t, err)
-
-	result, err = PostfixToPrefix("4 2 - 3 * 5 +")
-	assert.Equal(t, "+ * - 4 2 3 5", result)
-	assert.NoError(t, err)
-
-	result, err = PostfixToPrefix("6 5 4 - * 2 /")
-	assert.Equal(t, "/ * 6 - 5 4 2", result)
-	assert.NoError(t, err)
-
-	result, err = PostfixToPrefix("9 7 + 1 3 + / 2 * 3 - 8 +")
-	assert.Equal(t, "+ - * / + 9 7 + 1 3 2 3 8", result)
-	assert.NoError(t, err)
-
-	result, err = PostfixToPrefix("7 2 * 3 - 8 + 1 - 5 4 * 2 / +")
-	assert.Equal(t, "+ - + - * 7 2 3 8 1 / * 5 4 2", result)
-	assert.NoError(t, err)
-
-	result, err = PostfixToPrefix("2 5 ^ 9 - 12 + 6 2 / - 4 + 2 - 9 +")
-	assert.Equal(t, "+ - + - + - ^ 2 5 9 12 / 6 2 4 2 9", result)
-	assert.NoError(t, err)
-
-	result, err = PostfixToPrefix("6 2 / 2 * 2 - 8 2 ^ + 2 3 * - 7 3 ^ +")
-	assert.Equal(t, "+ - + - * / 6 2 2 2 ^ 8 2 * 2 3 ^ 7 3", result)
-	assert.NoError(t, err)
+	for _, tc := range []struct {
+		name        string
+		expression  string
+		expectedRes string
+		expectedErr error
+	}{
+		{
+			name:        "Empty string",
+			expression:  "",
+			expectedRes: "",
+			expectedErr: errors.New("expression not found"),
+		},
+		{
+			name:        "Invalid character",
+			expression:  "4 2 7 8 ! ? @",
+			expectedRes: "",
+			expectedErr: errors.New("invalid character entered"),
+		},
+		{
+			name:        "No whitespace",
+			expression:  "8 5+",
+			expectedRes: "",
+			expectedErr: errors.New("invalid character entered"),
+		},
+		{
+			name:        "Too many operators",
+			expression:  "4 2 - + -",
+			expectedRes: "",
+			expectedErr: errors.New("postfix expression entered incorrectly"),
+		},
+		{
+			name:        "No operators",
+			expression:  "4 2 5",
+			expectedRes: "",
+			expectedErr: errors.New("postfix expression entered incorrectly"),
+		},
+		{
+			name:        "Fractional numbers",
+			expression:  "4.1 2.5 +",
+			expectedRes: "+ 4.1 2.5",
+			expectedErr: nil,
+		},
+		{
+			name:        "Short expression",
+			expression:  "41 23 10 / +",
+			expectedRes: "+ 41 / 23 10",
+			expectedErr: nil,
+		},
+		{
+			name:        "Medium expression",
+			expression:  "9 7 + 1 3 + / 2 * 3 - 8 +",
+			expectedRes: "+ - * / + 9 7 + 1 3 2 3 8",
+			expectedErr: nil,
+		},
+		{
+			name:        "Long expression",
+			expression:  "6 2 / 2 * 2 - 8 2 ^ + 2 3 * - 7 3 ^ +",
+			expectedRes: "+ - + - * / 6 2 2 2 ^ 8 2 * 2 3 ^ 7 3",
+			expectedErr: nil,
+		},
+	} {
+		t.Run(tc.name, func(t *testing.T) {
+			res, err := PostfixToPrefix(tc.expression)
+			assert.Equal(t, tc.expectedRes, res)
+			assert.Equal(t, tc.expectedErr, err)
+		})
+	}
 }
 
 func ExamplePostfixToPrefix() {
-	prefixExpression, _ := PostfixToPrefix("6 3 ^ 3 4 ^ - 5 +")
-	fmt.Println(prefixExpression)
+	res, _ := PostfixToPrefix("6 3 ^ 3 4 ^ - 5 +")
+	fmt.Println(res)
 
 	// Output:
 	// + - ^ 6 3 ^ 3 4 5
